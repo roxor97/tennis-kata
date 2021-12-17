c56c69593887130f79e6e4ad93811aa684921e90
Roxor <junyperez97@gmail.com>
2021-12-17 01:17:36 -0500

    se hicieron pequeños cambios en la clase tennisGame3

--- src/main/java/TennisGame3.java
+++ src/main/java/TennisGame3.java
@@ -12,54 +12,55 @@ public class TennisGame3 implements TennisGame {
     }
 
     public String getScore() {
-        if (scoreIsLowerThan4()) {
+        if (scoreIsLowerThan4())
             return lowerThan4();
-        } else if (scorePlayer1 == scorePlayer2) {
+        else if (isScoreEqual())
             return "Deuce";
-        } else {
+        else{
             return greaterThan4();
         }
+    }
+
+    private boolean isScoreEqual() {
+        return scorePlayer1 == scorePlayer2;
     }
 
     private boolean scoreIsLowerThan4() {
         return bothScoreLowerThan4() && additionDifferentTo6();
     }
 
-    private boolean bothScoreLowerThan4() {
-        return scorePlayer1 < 4 && scorePlayer2 < 4;
-    }
-
     private boolean additionDifferentTo6() {
         return scorePlayer1 + scorePlayer2 != 6;
     }
 
+    private boolean bothScoreLowerThan4() {
+        return scorePlayer1 < 4 && scorePlayer2 < 4;
+    }
+
+    private String playerName() {
+        return  scorePlayer1 > scorePlayer2 ? player1Name : player2Name;
+    }
+
     private String greaterThan4() {
-        String playerName = getPlayerName();
-        return scoreAdvantageOrWin() ? "Advantage " + playerName : "Win for " + playerName;
+        return scoreAdvantageOrWin() ? "Advantage " + playerName() : "Win for " + playerName();
     }
 
     private boolean scoreAdvantageOrWin() {
-        return (scorePlayer1 - scorePlayer2) * (scorePlayer1 - scorePlayer2) == 1;
-    }
-
-    private String getPlayerName() {
-        String playerName;
-        playerName = scorePlayer1 > scorePlayer2 ? player1Name : player2Name;
-        return playerName;
+        return (scorePlayer1 - scorePlayer2)*(scorePlayer1 - scorePlayer2) == 1;
     }
 
     private String lowerThan4() {
-        String score;
+        String s;
         String[] p = new String[]{"Love", "Fifteen", "Thirty", "Forty"};
-        score = p[scorePlayer1];
-        return (scorePlayer1 == scorePlayer2) ? score + "-All" : score + "-" + p[scorePlayer2];
+        s = p[scorePlayer1];
+        return (isScoreEqual()) ? s + "-All" : s + "-" + p[scorePlayer2];
     }
 
     public void wonPoint(String playerName) {
-        if (playerName.equals(this.player1Name))
-            this.scorePlayer1++;
+        if (playerName.equals(player1Name))
+            this.scorePlayer1 ++;
         else
-            this.scorePlayer2++;
-    }
+            this.scorePlayer2 ++;
 
+    }
 }
