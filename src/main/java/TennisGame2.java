e2d5e8b4fdf434f5dd4e9be68bab41d6a2bc4a33
Roxor <junyperez97@gmail.com>
2021-12-17 01:01:01 -0500

    se baja la complejidad al codigo

--- src/main/java/TennisGame2.java
+++ src/main/java/TennisGame2.java
@@ -3,9 +3,6 @@ public class TennisGame2 implements TennisGame
 {
     public int p1Point = 0;
     public int p2Point = 0;
-    
-    public String p1Res = "";
-    public String p2Res = "";
     private String player1Name;
     private String player2Name;
 
@@ -22,57 +19,19 @@ public class TennisGame2 implements TennisGame
         else if (p1Point >= 4 ||p2Point >= 4)
             score = scoreGreaterThan4();
 
-        if (p1Point > 0 && p2Point ==0)
-        {
-            if (p1Point ==1)
-                p1Res = "Fifteen";
-            if (p1Point ==2)
-                p1Res = "Thirty";
-            if (p1Point ==3)
-                p1Res = "Forty";
-            
-            p2Res = "Love";
-            score = p1Res + "-" + p2Res;
-        }
-        if (p2Point > 0 && p1Point ==0)
-        {
-            if (p2Point ==1)
-                p2Res = "Fifteen";
-            if (p2Point ==2)
-                p2Res = "Thirty";
-            if (p2Point ==3)
-                p2Res = "Forty";
-            
-            p1Res = "Love";
-            score = p1Res + "-" + p2Res;
-        }
-        
-        if (p1Point > p2Point && p1Point < 4)
-        {
-            if (p1Point ==2)
-                p1Res ="Thirty";
-            if (p1Point ==3)
-                p1Res ="Forty";
-            if (p2Point ==1)
-                p2Res ="Fifteen";
-            if (p2Point ==2)
-                p2Res ="Thirty";
-            score = p1Res + "-" + p2Res;
-        }
-        if (p2Point > p1Point && p2Point < 4)
-        {
-            if (p2Point ==2)
-                p2Res ="Thirty";
-            if (p2Point ==3)
-                p2Res ="Forty";
-            if (p1Point ==1)
-                p1Res ="Fifteen";
-            if (p1Point ==2)
-                p1Res ="Thirty";
-            score = p1Res + "-" + p2Res;
-        }
+        else score = differentScore(p1Point) + "-" + differentScore(p2Point);
 
         return score;
+    }
+
+    private String differentScore(int scorePlayer) {
+        switch(scorePlayer)
+        {
+            case 0: return "Love";
+            case 1: return "Fifteen";
+            case 2: return "Thirty";
+            default: return "Forty";
+        }
     }
 
     private String equalScore() {
@@ -110,7 +69,7 @@ public class TennisGame2 implements TennisGame
     }
 
     public void wonPoint(String player) {
-        if ("player1".equals(player))
+        if (player == "player1")
             P1Score();
         else
             P2Score();
